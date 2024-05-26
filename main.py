e4c2e8a8559b5ae5dd329e0d57ebc53f13db92ad
NathanSnail <47441022+NathanSnail@users.noreply.github.com>
2024-05-26 16:56:20 +1200

    works other than outputting file

--- main.py
+++ main.py
@@ -10,30 +10,31 @@ while i < len(l):
 	line = l[i]
 	i = i + 1
 	if "style" in line or "File" in line:
-		t = t + line
+		t += "\n" + line
 		lifetime = True
 		continue
 	if line == "|-":
-		print(t)
+		print(t[1:] + "\n|-")
 		t = ""
 		continue
 	if lifetime:
 		lifetime = False
 		line = line[1:]
-		print(line)
 
 		parts = line.split("||")
 		while len(parts) < 3:
 			next = l[i]
-			i = i + 1
+			i += 1
 			line = line + "|" + next
 			parts = line.split("||")
-		print(parts)
 		if len([x for x in parts[0] if x != " "]) == 0:
 			high, low = int(parts[2]), int(parts[2])
 		else:
 			low, high = int(parts[0]), int(parts[1])
-		print(low, high)
-		print("S:", compute(low, high, 100)[-1])
+		best = compute(low, high, 100)[-1]
+		print(best)
+		while line != "|":
+			i += 1
+			line = l[i]
 		continue
-	print("dead line", line)
+	t += "\n" + line
